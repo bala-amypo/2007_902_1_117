c4cd7d077c477543abe8b40dab71b0fdd6d5cd4b
bala-amypo <bala@amypo.in>
2025-12-26 09:02:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceProfileRepository.java
+++ src/main/java/com/example/demo/repository/DeviceProfileRepository.java
@@ -1,13 +1,9 @@
-package com.example.demo.repository;
+package com.example.demo;
 
-import com.example.demo.entity.DeviceProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
-public interface DeviceProfileRepository extends JpaRepository<DeviceProfile, Long> {
-
+public interface DeviceProfileService {
+    DeviceProfile registerDevice(DeviceProfile device);
     Optional<DeviceProfile> findByDeviceId(String deviceId);
-    List<DeviceProfile> findByUserId(Long userId);
+    DeviceProfile updateTrustStatus(Long id, Boolean trusted);
 }
