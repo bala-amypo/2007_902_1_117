dccc832d7fb660034db1f2385d81eca07cb1089a
bala-amypo <bala@amypo.in>
2025-12-26 04:59:19 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,3 @@
--- Active: 1766461130551@@127.0.0.1@3306@mysql
-package com.example.demo.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
