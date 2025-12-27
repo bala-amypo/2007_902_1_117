c08cb05a2fba5579b814ce32ba2a531f0115de1f
bala-amypo <bala@amypo.in>
2025-12-27 09:26:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
@@ -3,37 +3,40 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.LoginEvent;
 import com.example.demo.repository.LoginEventRepository;
 import com.example.demo.service.LoginEventService;
-import org.springframework.stereotype.Service;
 import com.example.demo.util.RuleEvaluationUtil;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+
 @Service
 public class LoginEventServiceImpl implements LoginEventService {
 
-    private final LoginEventRepository repo;
+    private final LoginEventRepository loginEventRepository;
     private final RuleEvaluationUtil evaluator;
-     private final LoginEventRepository loginEventRepository;
 
-    public LoginEventServiceImpl(LoginEventRepository repo, RuleEvaluationUtil evaluator, LoginEventRepository loginEventRepository) {
-        this.repo = repo;
-        this.evaluator = evaluator;
+    public LoginEventServiceImpl(
+            LoginEventRepository loginEventRepository,
+            RuleEvaluationUtil evaluator
+    ) {
         this.loginEventRepository = loginEventRepository;
+        this.evaluator = evaluator;
     }
 
     @Override
     public LoginEvent recordLogin(LoginEvent event) {
-        LoginEvent saved = repo.save(event);
+        LoginEvent saved = loginEventRepository.save(event);
         evaluator.evaluateLoginEvent(saved);
         return saved;
     }
 
     @Override
     public List<LoginEvent> getEventsByUser(Long userId) {
-        return repo.findByUserId(userId);
+        return loginEventRepository.findByUserId(userId);
     }
 
     @Override
     public List<LoginEvent> getSuspiciousLogins(Long userId) {
-        return repo.findByUserIdAndLoginStatus(userId, "FAILED");
+        return loginEventRepository.findByUserIdAndLoginStatus(userId, "FAILED");
     }
 
     @Override
