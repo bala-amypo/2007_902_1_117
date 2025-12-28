2d89bdd73fbed75d3b3fead175158669f0a26355
bala-amypo <bala@amypo.in>
2025-12-28 12:01:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceProfileRepository.java
+++ src/main/java/com/example/demo/repository/DeviceProfileRepository.java
@@ -3,8 +3,17 @@ package com.example.demo.repository;
 import com.example.demo.entity.DeviceProfile;
 import java.util.*;
 
-public interface DeviceProfileRepository {
-    DeviceProfile save(DeviceProfile device);
+// public interface DeviceProfileRepository {
+//     DeviceProfile save(DeviceProfile device);
+//     Optional<DeviceProfile> findById(Long id);
+//     Optional<DeviceProfile> findByDeviceId(String deviceId);
+// }
+
+
+@Repository
+public interface DeviceProfileRepository
+        extends JpaRepository<DeviceProfile, Long> {
+ DeviceProfile save(DeviceProfile device);
     Optional<DeviceProfile> findById(Long id);
     Optional<DeviceProfile> findByDeviceId(String deviceId);
 }
--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -3,10 +3,18 @@ package com.example.demo.repository;
 import com.example.demo.entity.PolicyRule;
 import java.util.*;
 
-public interface PolicyRuleRepository {
-    PolicyRule save(PolicyRule rule);
+// public interface PolicyRuleRepository {
+//     PolicyRule save(PolicyRule rule);
+//     List<PolicyRule> findAll();
+//     List<PolicyRule> findByActiveTrue();
+//     Optional<PolicyRule> findById(Long id);
+// }
+
+@Repository
+public interface PolicyRuleRepository
+        extends JpaRepository<PolicyRule, Long> {
+            PolicyRule save(PolicyRule rule);
     List<PolicyRule> findAll();
     List<PolicyRule> findByActiveTrue();
     Optional<PolicyRule> findById(Long id);
 }
-
