4eb9ac68506ed8b5c08299217715c821fb48c6f0
bala-amypo <bala@amypo.in>
2025-12-28 14:24:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoginEventRepository.java
+++ src/main/java/com/example/demo/repository/LoginEventRepository.java
@@ -1,13 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.LoginEvent;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 
+@Repository  // optional, but sometimes helps
+public interface LoginEventRepository extends JpaRepository<LoginEvent, Long> {
 
-public interface LoginEventRepository {
-    LoginEvent save(LoginEvent event);
     List<LoginEvent> findByUserId(Long userId);
+
     List<LoginEvent> findByUserIdAndLoginStatus(Long userId, String status);
 }
