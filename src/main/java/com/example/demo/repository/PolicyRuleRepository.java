7bd7db559dc8a5db0c1f61b8f2bb5841e462b2d0
bala-amypo <bala@amypo.in>
2025-12-28 16:37:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -1,12 +1,21 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.entity.PolicyRule;
+// import java.util.*;
+
+// public interface PolicyRuleRepository {
+//     PolicyRule save(PolicyRule rule);
+//     List<PolicyRule> findAll();
+//     List<PolicyRule> findByActiveTrue();
+//     Optional<PolicyRule> findById(Long id);
+// }
+
 package com.example.demo.repository;
 
 import com.example.demo.entity.PolicyRule;
-import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PolicyRuleRepository {
-    PolicyRule save(PolicyRule rule);
-    List<PolicyRule> findAll();
-    List<PolicyRule> findByActiveTrue();
-    Optional<PolicyRule> findById(Long id);
+@Repository
+public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
 }
-
