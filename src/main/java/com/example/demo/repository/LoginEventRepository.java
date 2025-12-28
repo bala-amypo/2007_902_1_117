12851418692dfc6244357310cf6c4d962602954b
bala-amypo <bala@amypo.in>
2025-12-28 11:55:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoginEventRepository.java
+++ src/main/java/com/example/demo/repository/LoginEventRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.LoginEvent;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface LoginEventRepository {
     LoginEvent save(LoginEvent event);
     List<LoginEvent> findByUserId(Long userId);
     List<LoginEvent> findByUserIdAndLoginStatus(Long userId, String status);
-
 }
